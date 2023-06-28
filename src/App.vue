509fd8bdedf9a25731864459a197ddb14fcb8387
Edoardo-Monti <122456302+Edoardo-Monti@users.noreply.github.com>
2023-06-28 17:03:34 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script >
+import {store} from './store'
+import MainComp from './components/MainComp.vue';
+
+export default{
+  name: 'App',
+  components: {
+    MainComp,
+  },
+  data(){
+    return{
+      store
+    }
+  }
+}
+
+</script>
+
+<template>
+  <div>
+    <h1 class="animate__animated animate__rubberBand">ciao</h1>
+    
+    <MainComp/>
+  </div>
+</template>
+
+<style lang="scss">
+@use "./style/main.scss";
+.animate__animated.animate__rubberBand{
+
+    animation-delay: 2s;
+}
+</style>
